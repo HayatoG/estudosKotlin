b5558231a725af3b15e1e19eb761a36499e9d0cc
HayatoG <guilherme.ryders@gmail.com>
2020-02-23 16:15:28 -0300

    Adicionando aulas 24 e 25 - Operador Elvis e Operador When

---
+++ kotlinUdemy/src/aula24.kt
@@ -0,0 +1,16 @@
+/*
+    [Aula 24] - Operador Elvis
+
+    Elvis trabalha com variáveis que podem ser nulas.
+    val test: Int? = null
+*/
+/*
+fun elvisOperation(){
+
+    val test: Int? = null
+    val op: Int = test ?: 100
+    println(op)
+
+
+}
+*/
