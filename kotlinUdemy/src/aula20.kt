c64c03f32f2e120c80935e9c725a2484a0144678
HayatoG <guilherme.ryders@gmail.com>
2020-02-28 10:06:18 -0300

    Atualizando o repo

--- kotlinUdemy/src/aula20.kt
+++ kotlinUdemy/src/aula20.kt
@@ -1,3 +1,24 @@
 /*
     [Aula 20] - Operações
 */
+
+fun operacoes(){
+
+    println("2 + 2 = ${2 + 2}")
+    println("2 - 2 = ${2 - 2}")
+    println("2 * 2 = ${2 * 2}")
+    println("2 / 2 = ${2 / 2}")
+    println("10 % 4 = ${10 % 4}")
+    println("10 % 2 = ${10 % 2}")
+
+    var numero =  10
+
+    println("numero ++ = ${numero++}")
+    println("numero -- = ${numero--}")
+    println("++numero = ${++numero}")
+    println("--numero = ${--numero}")
+
+    /*
+    Há também as opções: +=; -=; \=; *= e %=
+     */
+}
---
+++ kotlinUdemy/src/aula21.kt
@@ -0,0 +1,3 @@
+/*
+    [Aula 21] - Controle de fluxo (if - else) Pt.1 e Pt.2
+*/
