b2bfb93e1da1d4303b62efa43b6e51258f5adb07
HayatoG <guilherme.ryders@gmail.com>
2020-08-06 20:49:17 -0300

    Adicionando aula 26 - Controle de fluxo FOR

--- kotlinUdemy/src/main.kt
+++ kotlinUdemy/src/main.kt
@@ -1,10 +1,34 @@
 fun main() {
+    // for - loop
+    // loop infinito
 
-<<<<<<< HEAD
-=======
-    println(calcBonus("Coordenador", 1000f))
-    println(calcBonus("Gerente Junior", 30000f))
-    println(calcBonus("Gerente Sênior", 50000f))
->>>>>>> b5558231a725af3b15e1e19eb761a36499e9d0cc
+    //Alocou um espaço a cada caractere da String
+    val str = "Guilherme Oliveira"
+    for (c in str){
+        print("$c ")
+    }
+    print("\n")
 
+    //Ranges
+    for (i in 1..100){
+        print("$i\n")
+    }
+
+    //Com o step, dá pra ir de 2 em 2 (por exemplo)
+    for(i in 0..100 step 2){
+        print("$i\n")
+    }
+
+    //Assim, exibirá de 5 em 5, e caso i seja 80, ele não será exibido
+    for (i in 0..100 step 5){
+        if (i != 90){
+            print("$i ")
+        }
+    }
+    print("\n")
+    //Nesse caso, downTo é usado para decrescer
+    //O Step também pode ser aplicado neste caso
+    for (j in 100 downTo 0 step 10){
+        print("$j ")
+    }
 }
